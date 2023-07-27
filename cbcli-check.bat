f8b980781847b759b1461a2e0ff25d0ec690dfd4
Tarmo Jussila <tarmo.jussila@gmail.com>
2023-07-27 16:09:52 +0300

    Support for passing path variables as arguments to batch files

--- cbcli-check.bat
+++ cbcli-check.bat
@@ -6,6 +6,20 @@ for /f "tokens=1,* delims==" %%a in (cbcli-config.ini) do (
     set "%%a=%%b"
 )
 
+:: Argument variable parameters
+if not "%~1"=="" (
+    set "COMPILER_DIR_PATH=%~1"
+)
+if not "%~2"=="" (
+    set "SOURCE_FILE_PATH=%~2"
+)
+if not "%~3"=="" (
+    set "SOURCE_DIR_PATH=%~3"
+)
+if not "%~4"=="" (
+    set "TARGET_EXE_PATH=%~4"
+)
+
 :: Compiler-related path variables
 set "COMPILER_FILE_PATH=%COMPILER_DIR_PATH%\CBCompiler.exe"
 set "COMPILER_CONFIG_PATH=%COMPILER_DIR_PATH%\Compiler"
